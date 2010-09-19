1211697caf82c8026ee73a8a79e8f27f37346b04
Vivian McPhail <haskell.vivian.mcphail@gmail.com>
2010-09-19 14:24:30 +1200

    add parametric to Simple
    darcs-hash:20100919022430-af16d-5609e4e99e56376d5bd313693345f3316cb49f48.gz

--- lib/Graphics/Rendering/Plot/Figure/Simple.hs
+++ lib/Graphics/Rendering/Plot/Figure/Simple.hs
@@ -14,9 +14,12 @@
 
 module Graphics.Rendering.Plot.Figure.Simple (
                                               plot
+                                             , parametric
                                              ) where
 
 -----------------------------------------------------------------------------
+
+import Numeric.Vector(linspace,mapVector)
 
 import Graphics.Rendering.Plot.Figure
 
@@ -33,6 +36,19 @@ plot ds = do
                            addAxis YAxis (Side Lower) $ return ()
                            setRangeFromData XAxis Lower
                            setRangeFromData YAxis Lower
+
+-- | create a figure with a single parametric plot over n points
+--   with lower X and Y axes whose ranges are set from the data
+parametric :: (Double -> Double,Double -> Double) -> (Double,Double) -> Int -> Figure ()
+parametric (fx,fy) (l,h) n = do
+                             setPlots 1 1
+                             withPlot (1,1) $ do
+                                              let t = linspace n (l,h)
+                                              setDataset (Line,mapVector fx t,[mapVector fy t])
+                                              addAxis XAxis (Side Lower) $ return ()
+                                              addAxis YAxis (Side Lower) $ return ()
+                                              setRangeFromData XAxis Lower
+                                              setRangeFromData YAxis Lower
 
 -----------------------------------------------------------------------------
 
